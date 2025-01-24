7808597a711e3c93d0ca972848fdc2cf034b2dcc
maxxboutin <boutinm@oldcolony.info>
2025-01-24 13:04:29 -0500

    save

--- Session_3/src/ArrayListEquals/Main.java
+++ Session_3/src/ArrayListEquals/Main.java
@@ -6,14 +6,16 @@ public class Main
 {
     public static void main(String[] args) {
         //This code is just to test your equals method
-        ArrayList<Integer> list1 = new ArrayList<>() {{
+        ArrayList<Integer> list1 = new ArrayList<>()
+        {{
             add(10);
             add(9);
             add(5);
             add(2);
             add(9);
         }};
-        ArrayList<Integer> list2 = new ArrayList<>() {{
+        ArrayList<Integer> list2 = new ArrayList<>()
+        {{
             add(10);
             add(9);
             add(5);
@@ -22,8 +24,10 @@ public class Main
         }};
 
         // Test if list1 is equal to list 2
+        equals();
 
-        ArrayList<Integer> list3 = new ArrayList<>() {{
+        ArrayList<Integer> list3 = new ArrayList<>()
+        {{
             add(1);
             add(9);
             add(5);
@@ -32,6 +36,13 @@ public class Main
         }};
 
         // Test if list 2 is equal to list 3
+
+    }
+
+    public boolean equals(ArrayList<Integer> , ArrayList<Integer> list2)
+    {
+
+        for (int i = 0; i < a; i++) {}
 
     }
 
